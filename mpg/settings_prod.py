002c6669336b5393a84774e64b42ba46bda4f624
Brunno Vodola <bvodola@gmail.com>
2017-10-19 15:51:41 -0200

    Changes

--- mpg/settings_prod.py
+++ mpg/settings_prod.py
@@ -1,4 +1,4 @@
-import os
+    import os
 
 """
 Main Settings
@@ -28,11 +28,14 @@ DATABASES = {
 """
 Static files
 """
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATIC_PATH = os.path.join(BASE_DIR,'static')
+
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    STATIC_PATH,
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
