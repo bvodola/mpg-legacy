4549561f73d99b0d54fbfaf542b626d65fbe9b70
Brunno Vodola <bvodola@gmail.com>
2017-10-19 18:59:59 -0200

    Changes

--- mpg/settings_prod.py
+++ mpg/settings_prod.py
@@ -34,9 +34,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 #
 # # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
