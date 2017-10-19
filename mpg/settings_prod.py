7e15cd7af7cabf5c0995376f93b648e94523ec17
Brunno Vodola <bvodola@gmail.com>
2017-10-19 18:52:27 -0200

    Changes

--- mpg/settings_prod.py
+++ mpg/settings_prod.py
@@ -32,11 +32,11 @@ Static files
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# 
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
